868249814d6bf9b36b2f0234a4a69bfc1e45e416
a.bubelev <bubeleu@gmail.com>
2022-10-01 11:43:59 +0300

    array done

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const price = [2, 4, 3, 7, 5, 4, 7];
+const price = [6, 2, 5, 7, 8, 3, 4];
 
 const profit = (price) => {
     let fav = null;
+    let arr = [];
     for (let i = 0; i < price.length; i++) {
-        for (let j = 0; j < price.length; j++) {
-            let curMax = price[j] - price[j];
-            console.log(curMax)
+        let allDayProfit = []
+        for (let j = i+1; j < price.length; j++) {
+            let curMax = price[j] - price[i];
+            allDayProfit.push(curMax)
         }
+        console.log(allDayProfit)
     }
+  
     return fav
 }
-console.log(profit(price))
+// console.log(profit(price))
+profit(price)
