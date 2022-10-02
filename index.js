adbf6ae5bb01af163fcd07720f7633b4edd78925
a.bubelev <bubeleu@gmail.com>
2022-10-02 15:03:51 +0300

    Math min in array

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const price = [6, 2, 5, 9, 8, 3, 6];
-const maxProfit = (price) => {
-    const result = {};
-    let minPrice = Math.min.apply(null, price);
-    let maxPrice = Math.max.apply(null, price);
-    let arr = [];
-    let profit = null;
-    for (let i = 0; i < price.length - 1; i++) {
-        let allDayProfit = []
-        for (let j = i + 1; j < price.length; j++) {
-            let curMax = price[j] - price[i];
-            allDayProfit.push(curMax)
-        }
-        profit = allDayProfit.reduce((cur, prev) => { return cur > prev ? cur : prev }, 0);
-        arr.push(profit)
-    }
-    let bestProfit = Math.max.apply(null, arr);
-    result.bestDayBuy = price.indexOf(minPrice) + 1;
-    result.bestDaySell = price.indexOf(maxPrice) + 1;
-    result.profit = bestProfit;
-    return result;
-}
+// const price = [6, 2, 5, 9, 8, 3, 6];
+// const maxProfit = (price) => {
+//     const result = {};
+//     let minPrice = Math.min.apply(null, price);
+//     let maxPrice = Math.max.apply(null, price);
+//     let arr = [];
+//     let profit = null;
+//     for (let i = 0; i < price.length - 1; i++) {
+//         let allDayProfit = []
+//         for (let j = i + 1; j < price.length; j++) {
+//             let curMax = price[j] - price[i];
+//             allDayProfit.push(curMax)
+//         }
+//         profit = allDayProfit.reduce((cur, prev) => { return cur > prev ? cur : prev }, 0);
+//         arr.push(profit)
+//     }
+//     let bestProfit = Math.max.apply(null, arr);
+//     result.bestDayBuy = price.indexOf(minPrice) + 1;
+//     result.bestDaySell = price.indexOf(maxPrice) + 1;
+//     result.profit = bestProfit;
+//     return result;
+// }
 
-console.log(maxProfit(price))
+// console.log(maxProfit(price))
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min.apply(null,args)
+    }
+  }
+
+  const num = new SmallestIntegerFinder;
+  console.log(num.findSmallestInt([1,2,3,4,5,6]))
