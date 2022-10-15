0fe3ad5705e6ef765809075d1615ef9cc4905bd1
a.bubelev <bubeleu@gmail.com>
2022-10-15 17:40:39 +0300

    carry

--- index.js
+++ index.js
@@ -95,9 +95,21 @@
 // }
 // console.log(polidrom(str))
 //////////////////////////////////////////////////////////////////////////
-function sum(x) {
-    if (arguments.length == 2) {
-        return arguments[0] + arguments[1]
+// function sum(x) {
+//     if (arguments.length == 2) {
+//         return arguments[0] + arguments[1]
+//     } else {
+//         return function (y) {
+//             return x + y
+//         }
+//     }
+// }
+// console.log(sum(2, 3));   // Outputs 5
+// console.log(sum(2)(3));
+//////////////////////////////////////////////////////////////////////////
+function sum(x, y) {
+    if (y !== undefined) {
+        return x + y
     } else {
         return function (y) {
             return x + y
@@ -106,8 +118,6 @@ function sum(x) {
 }
 console.log(sum(2, 3));   // Outputs 5
 console.log(sum(2)(3));
-//////////////////////////////////////////////////////////////////////////
-
 
 
 
