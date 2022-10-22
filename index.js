0c679cc854e8f633a94c3857440495e769eb4a38
a.bubelev <bubeleu@gmail.com>
2022-10-22 16:57:43 +0300

    fix

--- index.js
+++ index.js
@@ -9,9 +9,19 @@
 
 
 //////////////////////////////////////////////////////////////////////////
-
-
-
+// var myObject = {
+//     foo: "bar",
+//     func: function() {
+//         var self = this;
+//         console.log("outer func:  this.foo = " + this.foo);//bar
+//         console.log("outer func:  self.foo = " + self.foo);//bar
+//         (function() {
+//             console.log("inner func:  this.foo = " + this.foo);//undefined
+//             console.log("inner func:  self.foo = " + self.foo);//bar
+//         }());
+//     }
+// };
+// myObject.func();
 //////////////////////////////////////////////////////////////////////////
 // (function(){
 //     var a = b = 3;
