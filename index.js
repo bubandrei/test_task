b9a40a6f0cc54b269deaf09dc87a8438d52ae034
a.bubelev <bubeleu@gmail.com>
2022-10-30 18:06:11 +0300

    check

--- index.js
+++ index.js
@@ -1,3 +1,185 @@
+//////////////////////////////////////////////////////////////////////////
+// var b = 1;
+// function outer(){
+//    	var b = 2
+//     function inner(){
+//         b++;
+//         var b = 3;
+//         console.log(b)//3
+//     }
+//     inner();
+// }
+// outer();
+//////////////////////////////////////////////////////////////////////////
+// console.log(typeof typeof 1);//string
+//////////////////////////////////////////////////////////////////////////
+// console.log(1 < 2 < 3);//true<3(1<3)
+// console.log(3 > 2 > 1);//true>1(1>1) false
+//////////////////////////////////////////////////////////////////////////
+// for (let i = 0; i < 5; i++) {
+//     setTimeout(function() { console.log(i); }, i * 1000 );
+//   }//0,1,2,3,4
+//////////////////////////////////////////////////////////////////////////
+// var x = 21;
+// var girl = function () {
+//     console.log(x);//undefind
+//     var x = 20;
+// };
+// girl ();
+//////////////////////////////////////////////////////////////////////////
+// (function () {
+//     try {
+//         throw new Error();
+//     } catch (x) {
+//         var x = 1, y = 2;
+//         console.log(x);//1
+//     }
+//     console.log(x);//undefind
+//     console.log(y);//2
+// })();
+//////////////////////////////////////////////////////////////////////////
+// var length = 10;
+// function fn() {
+// 	console.log(this.length);
+// }
+
+// var obj = {
+//   length: 5,
+//   method: function(fn) {
+//     fn();//10
+//     arguments[0]();//2
+//   }
+// };
+// obj.method(fn, 1);
+//////////////////////////////////////////////////////////////////////////
+// var hero = {
+//     _name: 'John Doe',
+//     getSecretIdentity: function (){
+//         return this._name;
+//     }
+// };
+// var stoleSecretIdentity = hero.getSecretIdentity;
+// console.log(stoleSecretIdentity.apply(hero));//'John Doe'
+// console.log(stoleSecretIdentity());//undefind
+// console.log(hero.getSecretIdentity());//'John Doe'
+//////////////////////////////////////////////////////////////////////////
+// (function(x) {
+//     return (function(y) {
+//         console.log(x);//1
+//     })(2)
+// })(1);
+//////////////////////////////////////////////////////////////////////////
+// console.log((function f(n) { return ((n > 1) ? n * f(n - 1) : n) })(10));
+//10*(9*(8*(7*(6*(5*(4*(3*(2*(1)))))))))
+//////////////////////////////////////////////////////////////////////////
+// var a={},
+//     b={key:'b'},
+//     c={key:'c'};
+
+// a[b]=123;//a{Objectobject:123}
+// a[c]=456;//a{Objectobject:456}
+
+// console.log(a[b]);//456
+//////////////////////////////////////////////////////////////////////////
+// console.log(false == '0')//true
+// console.log(false === '0')//false (type)
+//////////////////////////////////////////////////////////////////////////
+// console.log("0 || 1 = "+(0 || 1));//1
+// console.log("1 || 2 = "+(1 || 2));//1
+// console.log("0 && 1 = "+(0 && 1));//0
+// console.log("1 && 2 = "+(1 && 2));//2
+//////////////////////////////////////////////////////////////////////////
+// var list = readHugeList();
+// var nextListItem = function () {
+//     var item = list.pop();
+//     if (item) {
+//         setTimeout(nextListItem, 0)
+//     }
+// };
+//////////////////////////////////////////////////////////////////////////
+// console.log(1 +  "2" + "2");//122
+// console.log(1 +  +"2" + "2");//32
+// console.log(1 +  -"1" + "2");//02
+// console.log(+"1" +  "1" + "2");//112
+// console.log( "A" - "B" + "2");//NaN2
+// console.log( "A" - "B" + 2);//NaN
+//////////////////////////////////////////////////////////////////////////
+// var arr1 = "john".split('');//[j,o,h,n]
+// var arr2 = arr1.reverse();//[n,h,o,j]
+// var arr3 = "jones".split('');//[j,o,n,e,s]
+// arr2.push(arr3);//[n,h,o,j,[j,o,n,e,s]]
+// console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));//5 [j,o,n,e,s]
+// console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));//5 [j,o,n,e,s]
+//////////////////////////////////////////////////////////////////////////
+// var d = {};
+// [ 'zebra', 'horse' ].forEach(function(k) {
+// 	d[k] = undefined;
+// });
+// console.log(d)
+//////////////////////////////////////////////////////////////////////////
+// const sum = (a, b) => {
+//     if (b != undefined) {
+//         return a + b
+//     }
+//     return function (b) {
+//         return a + b
+//     }
+// }
+// function sum (a) {
+//     if (arguments.length == 2) {
+//         return arguments[0] + arguments[1];
+//     }
+//     return function (b) {
+//         return a + b
+//     }
+// }
+// console.log(sum(2, 5));   // Outputs 5
+// console.log(sum(2)(1));  // Outputs 5
+//////////////////////////////////////////////////////////////////////////
+// (function() {
+//     console.log(1); //1
+//     setTimeout(function(){console.log(2)}, 1000); //4
+//     setTimeout(function(){console.log(3)}, 0); //3
+//     console.log(4);//2
+// })();
+//////////////////////////////////////////////////////////////////////////
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 == 0.3);//false
+//////////////////////////////////////////////////////////////////////////
+// function foo1() {
+//     return {
+//         bar: "hello"
+//     };
+// }
+
+// function foo2() {
+//     return//
+//     {
+//         bar: "hello"
+//     };
+// }
+// console.log(foo2())//undefind
+//////////////////////////////////////////////////////////////////////////
+// var myObject = {
+//     foo: "bar",
+//     func: function() {
+//         var self = this;
+//         console.log("outer func:  this.foo = " + this.foo);//bar
+//         console.log("outer func:  self.foo = " + self.foo);//bar
+//         (function() {
+//             console.log("inner func:  this.foo = " + this.foo);//undefind
+//             console.log("inner func:  self.foo = " + self.foo);//bar
+//         }());
+//     }
+// };
+// myObject.func();
+//////////////////////////////////////////////////////////////////////////
+// (function(){
+//     var a = b = 3;
+//   })();
+//   console.log("a defined? " + (typeof a !== 'undefined'));//referenceError false
+//   console.log("b defined? " + (typeof b !== 'undefined'));//3 true
+//////////////////////////////////////////////////////////////////////////
     // function zeroAndOne(s) {
     //   let cnt = 0;
     //   for (let i=0; i<s.length; i++){
