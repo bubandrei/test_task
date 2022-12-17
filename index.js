5e56b5499ea183ec6482ba0124a3113534b794cc
a.bubelev <bubeleu@gmail.com>
2022-12-17 13:33:25 +0300

    Set, concat

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function longest(s1, s2) {
+return [...new Set(s1+s2)].sort().join('')
+}
+console.log(longest("aretheyhere", "yestheyarehere"))
 
 //////////////////////////////////////////////////////////////////////////
 // function getSum(a, b) {
