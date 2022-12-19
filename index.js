ea7ee85dcd9013be0f424dfb6616ff782b0830cf
a.bubelev <bubeleu@gmail.com>
2022-12-19 14:45:03 +0300

    math.pow

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3)
+}
+
+console.log(rowSumOddNumbers(42))
+
+
+
+
+
 //////////////////////////////////////////////////////////////////////////
 // function findOdd(A) {
 //   let count = 0;
